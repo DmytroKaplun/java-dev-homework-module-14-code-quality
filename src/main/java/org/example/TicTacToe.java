7e8851907416f401635af63b35a8e2f13c02a63b
DmytroKaplun <newtonknight1990@gmail.com>
2024-04-13 22:21:24 +0300

    TicTacToe class added

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.util.logging.Logger;
 
-public class App {
-    private static final Logger logger = Logger.getLogger(App.class.getName());
+public class TicTacToe {
+    private static final Logger logger = Logger.getLogger(TicTacToe.class.getName());
 
-    public static void main(String[] args) {
+    public void run() {
         Scanner scanner = new Scanner(System.in);
 
         char[][] board = {
@@ -36,10 +36,10 @@ public class App {
         scanner.close();
     }
 
-    private static void playerTurn(char[][] board, Scanner scanner) {
+    private void playerTurn(char[][] board, Scanner scanner) {
         while (true) {
             if (scanner.hasNextInt()) {
-               int userInput = scanner.nextByte();
+                int userInput = scanner.nextByte();
 
                 if (userInput > 0 && userInput < 10) {
                     if (isSpaceEmpty(userInput, board)) {
@@ -56,7 +56,7 @@ public class App {
         }
     }
 
-    private static void computerTurn(char[][] board) {
+    private void computerTurn(char[][] board) {
         byte randomNumber;
         do {
             randomNumber = (byte) (Math.random() * (9 - 1 + 1) + 1);
@@ -64,7 +64,7 @@ public class App {
         placeSymbol(board, randomNumber, 'O');
     }
 
-    private static boolean isGameFinished(char[][] board) {
+    private boolean isGameFinished(char[][] board) {
         if (isWinner(board, 'X')) {
             printBoard(board);
             logger.info("You won the game!\nThanks for playing!");
@@ -88,7 +88,7 @@ public class App {
         return true;
     }
 
-    private static boolean isWinner(char[][] board, char symbol) {
+    private boolean isWinner(char[][] board, char symbol) {
         return (board[0][0] == symbol && board[0][1] == symbol && board[0][2] == symbol) ||
                 (board[1][0] == symbol && board[1][1] == symbol && board[1][2] == symbol) ||
                 (board[2][0] == symbol && board[2][1] == symbol && board[2][2] == symbol) ||
@@ -101,13 +101,13 @@ public class App {
                 (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol);
     }
 
-    private static void placeSymbol(char[][] board, int num, char symbol) {
+    private void placeSymbol(char[][] board, int num, char symbol) {
         int row = (num - 1) / 3;
         int col = (num - 1) % 3;
         board[row][col] = symbol;
     }
 
-    private static void printBoard(char[][] board) {
+    private void printBoard(char[][] board) {
         for (String s : Arrays.asList(board[0][0] + " | " + board[0][1] + " | " + board[0][2],
                 "---------", board[1][0] + " | " + board[1][1] + " | " + board[1][2], "---------",
                 board[2][0] + " | " + board[2][1] + " | " + board[2][2])) {
@@ -115,13 +115,13 @@ public class App {
         }
     }
 
-    private static void setEmptyBoard(char[][] board) {
+    private void setEmptyBoard(char[][] board) {
         for (char[] chars : board) {
             Arrays.fill(chars, ' ');
         }
     }
 
-    private static boolean isSpaceEmpty(int input, char[][] board) {
+    private boolean isSpaceEmpty(int input, char[][] board) {
         int row = (input - 1) / 3;
         int col = (input - 1) % 3;
         return board[row][col] == ' ';
